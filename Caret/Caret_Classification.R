6cd3f04031b7f8c1f6ecca9673f35127d501ae9a
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-02-28 14:33:14 +0000

    Update Caret_Classification.R

--- Caret/Caret_Classification.R
+++ Caret/Caret_Classification.R
@@ -1,3 +1,4 @@
+
 library(caret)
 library(minDiff)
 library(dplyr)
@@ -7,20 +8,26 @@ library(stringr)
 library(ggplot2)
 library(pROC)
 
+#setwd("C:/Users/mk693/OneDrive - University of Exeter/Desktop/2021/NIH/Data/classification/Blood.MultiOmics")
 source("Scripts/Caret.Classification.Functions.R")
-
 args <- commandArgs(T)
 
-data.feature.file=args[1]
-data.class.file=args[2]
-class.column=args[3]
-grouping.columns.num=args[4]
-grouping.columns.fact=args[5]
-scale=args[6]
-feature.selection=ifelse(trimws(tolower(args[7]),which = "both")=="yes",T,F)
-method=args[8]
+data.feature.file= args[1] #"Raw/NorCog.TotalRNA.logCPM.Scaled0.csv"
+data.class.file= args[2] #"Raw/NorCog.Pheno.csv"
+OutPrefix = args[3] #"NorCog.TotalRNA"
+class.column=args[4] #"Phenotype"
+grouping.columns.num=args[5] #"Age"
+grouping.columns.fact=args[6] #"Sex"
+scale=ifelse(tolower(trimws(args[7]))=="yes",T,F) #F
+feature.selection=ifelse(tolower(trimws(args[8]))=="yes",T,F) #T
+saveModel=ifelse(tolower(trimws(args[7]))=="yes",T,F)
+model= args[9] #"adaboost,awtan,bartMachine,bayesglm,earth,extraTrees,gaussprLinear,gbm,glm,glmnet,gpls,kknn,LMT,lssvmLinear,lssvmPoly,lssvmRadial,rf,RRF,svmBoundrangeString,svmExpoString,svmLinear,svmPoly,svmRadial"
 
 ######################## Reading Inputs ############################################
+sink(paste0(OutPrefix,".log"))
+
+cat("Reading inputs...")
+cat("\n")
 data.feature <- fread(file = data.feature.file , stringsAsFactors = F , header = T)
 rownames(data.feature) <- data.feature[,1]
 data.feature <- data.feature[,-1]
@@ -32,13 +39,15 @@ if(!identical(rownames(data.class),colnames(data.feature)))
 data.feature <- as.data.frame(t(data.feature))
 ######################## Scaling ###################################################
 if(scale){
-  
+  cat("Scalling feature data...")
+  cat("\n")
   data.feature <- as.data.frame(scale(data.feature))
 }
 
 ######################## Feature Selection #########################################
 if(feature.selection){
-  
+  cat("Feature selection...")
+  cat("\n")
   nzv <- nearZeroVar(data.feature, saveMetrics= TRUE)
   data.feature <- data.feature[,!nzv$nzv]
   
@@ -48,6 +57,8 @@ if(feature.selection){
 }
 
 ######################## Train-Test splitting ######################################
+cat("Train-Test splitting...")
+cat("\n")
 grouping.columns.num <- str_trim(str_split_1(grouping.columns.num , pattern = ","),side = "both")
 grouping.columns.fact <- str_trim(str_split_1(grouping.columns.fact , pattern = ","),side = "both")
 
@@ -56,8 +67,18 @@ data.class <- train.test.split(grouping.data = data.class , factor.grouping.vari
 data.train <- cbind.data.frame(data.feature[data.class$Train ,],Class=data.class[data.class$Train , class.column])
 data.test <- cbind.data.frame(data.feature[!data.class$Train ,],Class=data.class[!data.class$Train , class.column])
 ######################## Training ##################################################
+cat("Training...")
+cat("\n")
 model.all <-  getModelInfo()
-model.label <- model.all[[method]]$label
+model.label <- model.all[[model]]$label
+
+#caret.models <- modelLookup()
+#caret.models$label.model = NA
+#for (i in 1:nrow(caret.models)) {
+#  caret.models$label.model[i] = model.all[[caret.models$model[i]]]$label
+#}
+#caret.models <- caret.models[,c(1,7,2,3,4,5,6)]
+#write.csv(caret.models,file = "Raw/Caret.Models.csv", row.names = F)
 
 set.seed(1234)
 fitControl <- trainControl(
@@ -70,18 +91,23 @@ fitControl <- trainControl(
   #summaryFunction = twoClassSummary,
   selectionFunction = tolerance)
 
-Grid <- model.all[[method]]$grid(x = data.train[,-ncol(data.train)],y = data.train[,ncol(data.train)],len = 10)
+Grid <- model.all[[model]]$grid(x = data.train[,-ncol(data.train)],y = data.train[,ncol(data.train)],len = 10)
 
 fit <- train(Class ~ ., data = data.train, 
-             method = method, 
+             method = model, 
              trControl = fitControl,
              #metric="ROC",
              tuneGrid = Grid,
              verbose = F
              )
 
+if(saveModel)
+  saveRDS(fit,file = paste0(OutPrefix,".",model,".rds"))
+cat("Plot training results...")
+cat("\n")
+tiff(filename = paste(OutPrefix,".",model.label,".parameters.tuning.tiff"), units = "in", height = 10,width = 10,res = 300)
 ggplot(fit) + ggtitle(paste(model.label,"- parameters tuning"))+ theme_bw()
-
+graphics.off()
 folds <- unique(fit$pred$Resample)
 train.roc.obj <- vector(mode = "list", length = length(folds))
 train.roc.labels <- vector(mode = "character", length = length(folds))
@@ -90,24 +116,25 @@ for(i in 1:length(folds)){
   train.roc.obj[[i]] <- roc(response=fit$pred$obs[fit$pred$Resample==folds[i]],predictor=fit$pred[,4][fit$pred$Resample==folds[i]])
   train.roc.labels[i] <- paste0(folds[i]," (AUC=",round(train.roc.obj[[i]]$auc,digits = 2),")")
 }
-
+tiff(filename = paste(OutPrefix,".",model.label,"accuracy.on.train.data.tiff"), units = "in", height = 10,width = 10,res = 300)
 ggroc(train.roc.obj) + ggtitle(paste(model.label,"accuracy on train data")) + labs(color = "Fold") +
                        scale_color_discrete(labels=train.roc.labels)+ theme_bw()
-
+graphics.off()
 ####################### Testing ####################################################
+cat("Applying the model on test data...")
+cat("\n")
 pred <- predict(fit , newdata = data.test , type = "prob")
 pred$Predict <- predict(fit , newdata = data.test , type = "raw")
 pred$Reference <- data.test$Class
 
+cat("Plot test results...")
+cat("\n")
 obj.roc <- roc(response=pred$Reference,predictor=pred[,1])
+tiff(filename = paste(OutPrefix,".",model.label,"accuracy.on.test.data.tiff"), units = "in", height = 10,width = 10,res = 300)
 ggroc(obj.roc)+ggtitle(paste(model.label,"accuracy on test data")) +
   theme_bw() + annotate("text",x = 0.1,y = 0.1,label=paste0("AUC=",round(obj.roc$auc,digits = 2)))
-
+graphics.off()
 cat("Performance summary on test data:")
 confusionMatrix(data =as.factor(pred$Predict) , reference = as.factor(pred$Reference))
 
-
-
-
-
-
+sink()
