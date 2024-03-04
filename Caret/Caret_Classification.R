238c33fe55b95b427bc326bce9e64e8fae8298ee
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-03-04 13:21:32 +0000

    Update Caret_Classification.R

--- Caret/Caret_Classification.R
+++ Caret/Caret_Classification.R
@@ -18,15 +18,35 @@ OutPrefix = args[3]
 class.column=args[4] 
 grouping.columns.num=args[5] 
 grouping.columns.fact=args[6] 
-scale=ifelse(tolower(trimws(args[7]))=="yes",T,F) 
-feature.selection=ifelse(tolower(trimws(args[8]))=="yes",T,F) 
-saveModel=ifelse(tolower(trimws(args[7]))=="yes",T,F)
-models= str_split(trimws(args[9]),pattern = ",",simplify = T)[1,]
+scale=args[7] 
+feature.selection=args[8]
+saveModel=args[9]
+models= args[10]
+Do.Parellel=args[11]
 
+######################### Input Arguments ###########################################
+cat("Feature data file: ",data.feature.file,"\n")
+cat("Class labels file: ",data.class.file,"\n")
+cat("Output files prefix: ",OutPrefix,"\n")
+cat("Class variable: ",class.column,"\n")
+cat("Numeric variables for train-test split: ",grouping.columns.num,"\n") 
+cat("Categorical variables for train-test split: ",grouping.columns.fact,"\n")
+cat("Do you want to scale data? ",scale,"\n")
+cat("Do you want to run feature selection? ",feature.selection,"\n") 
+cat("Do you want to save trained model? ",saveModel,"\n")
+cat("Models: ",models,"\n")
+cat("Do you want to run training in parallel?",Do.Parellel,"\n")
 ######################## Reading Inputs ############################################
-
+cat("\n")
 cat("Reading inputs...")
 cat("\n")
+
+scale=ifelse(tolower(trimws(scale))=="yes",T,F) 
+feature.selection=ifelse(tolower(trimws(feature.selection))=="yes",T,F) 
+saveModel=ifelse(tolower(trimws(saveModel))=="yes",T,F)
+models= str_split(trimws(models),pattern = ",",simplify = T)[1,]
+Do.Parellel=ifelse(tolower(trimws(Do.Parellel))=="yes",T,F)
+
 data.feature <- fread(file = data.feature.file , stringsAsFactors = F , header = T)
 rownames(data.feature) <- data.feature[,1]
 data.feature <- data.feature[,-1]
@@ -69,11 +89,12 @@ data.test <- cbind.data.frame(data.feature[!data.class$Train ,],Class=as.factor(
 
 model.all <-  getModelInfo()
 for(i in 1:length(models)){
-  model <- models[i]
-  ######################## Training #################################################
-  model.label <- model.all[[model]]$label
-  cat(paste0("Train ",model," (",model.label,") ","on train data..."))
-  cat("\n")
+    model <- models[i]
+    ######################## Training #################################################
+    model.label <- model.all[[model]]$label
+    
+    cat(paste0("Train ",model," (",model.label,") ","model on train data..."))
+    cat("\n")
   
   set.seed(1234)
   fitControl <- trainControl(
@@ -81,62 +102,93 @@ for(i in 1:length(models)){
     number = 5, 
     classProbs = T,
     savePredictions = T,
-    selectionFunction = tolerance)
+    selectionFunction = best,
+    allowParallel = Do.Parellel,
+    verboseIter = F)
   
-  Grid <- model.all[[model]]$grid(x = data.train[,-ncol(data.train)],y = data.train[,ncol(data.train)],len = 10)
-  
-  fit <- train(Class ~ ., data = data.train, 
-               method = model, 
-               trControl = fitControl,
-               tuneGrid = Grid,
-               )
-  
-  if(saveModel)
-    saveRDS(fit,file = paste0(OutPrefix,".",model,".rds"))
-  cat("Plot training results...")
-  cat("\n")
-  if(nrow(Grid)>1){
-    tiff(filename = paste0(OutPrefix,".",model,".parameters.tuning.tiff"), units = "in", height = 6,width = 6,res = 300)
-    p <- ggplot(fit) + ggtitle(paste(model.label,"- parameters tuning"))+ theme_bw()
+  tryCatch(expr = {
+    
+    if(Do.Parellel){
+      library(doParallel)
+      cores <- detectCores()
+      cl <- makePSOCKcluster(cores)
+      registerDoParallel(cl)
+    }
+    
+    Grid <- model.all[[model]]$grid(x = data.train[,-ncol(data.train)],y = data.train[,ncol(data.train)],len = 10)
+    fit <- train(Class ~ ., data = data.train, 
+                 method = model, 
+                 trControl = fitControl,
+                 tuneGrid = Grid
+                 )
+    
+    if(Do.Parellel)
+      stopCluster(cl)
+    
+    if(saveModel)
+      saveRDS(fit,file = paste0(OutPrefix,".",model,".rds"))
+    cat("Plot training results...")
+    cat("\n")
+    if(nrow(Grid)>1){
+      p <- ggplot(fit) + ggtitle(paste(model.label,"- parameters tuning"))+ theme_bw()
+      tiff(filename = paste0(OutPrefix,".",model,".parameters.tuning.tiff"),type="cairo", units = "in", height = 10,width = 10,res = 300)
+      print(p)
+      graphics.off()
+    }
+    
+    fit.pred <- fit$pred[!is.na(fit$pred$pred),]
+    folds <- unique(fit.pred$Resample)
+    train.roc.obj <- vector(mode = "list", length = length(folds))
+    train.roc.labels <- vector(mode = "character", length = length(folds))
+    names(train.roc.obj) <- folds
+    
+    for(j in 1:length(folds)){
+      train.roc.obj[[j]] <- roc(response=fit.pred$obs[fit.pred$Resample==folds[j]],predictor=fit.pred[,4][fit.pred$Resample==folds[j]])
+      train.roc.labels[j] <- paste0(folds[j]," (AUC=",round(train.roc.obj[[j]]$auc,digits = 2),")")
+    }
+    
+    p <- ggroc(train.roc.obj) + ggtitle(paste(model.label,"accuracy on train data")) + labs(color = "Fold") +
+      scale_color_discrete(labels=train.roc.labels)+ theme_bw()
+    
+    tiff(filename = paste0(OutPrefix,".",model,".accuracy.on.train.data.tiff"),type="cairo", units = "in", height = 10,width = 10,res = 300)
     print(p)
     graphics.off()
-  }
-  
-  folds <- unique(fit$pred$Resample)
-  train.roc.obj <- vector(mode = "list", length = length(folds))
-  train.roc.labels <- vector(mode = "character", length = length(folds))
-  names(train.roc.obj) <- folds
-  for(j in 1:length(folds)){
-    train.roc.obj[[j]] <- roc(response=fit$pred$obs[fit$pred$Resample==folds[j]],predictor=fit$pred[,4][fit$pred$Resample==folds[j]])
-    train.roc.labels[j] <- paste0(folds[j]," (AUC=",round(train.roc.obj[[j]]$auc,digits = 2),")")
-  }
-  tiff(filename = paste0(OutPrefix,".",model,".accuracy.on.train.data.tiff"), units = "in", height = 6,width = 6,res = 300)
-  ggroc(train.roc.obj) + ggtitle(paste(model.label,"accuracy on train data")) + labs(color = "Fold") +
-                         scale_color_discrete(labels=train.roc.labels)+ theme_bw()
-  graphics.off()
-  imp.var <- varImp(fit ,scale=T,useModel=F)
-  tiff(filename = paste0(OutPrefix,".",model,".variable.importance.tiff"), units = "in", height = 6,width = 6,res = 300)
-  plot(imp.var, top = 20)
-  graphics.off()
-  ####################### Testing ####################################################
-  cat(paste0("Applying the ",model," model on test data..."))
+    
+    imp.var <- varImp(fit ,scale=T,useModel=F)
+    p <- plot(imp.var, top = 20)
+    
+    tiff(filename = paste0(OutPrefix,".",model,".variable.importance.tiff"),type="cairo", units = "in", height = 10,width = 10,res = 300)
+    print(p)
+    graphics.off()
+    ####################### Testing ####################################################
+    cat(paste0("Applying the ",model," model on test data..."))
+    cat("\n")
+    pred <- predict(fit , newdata = data.test , type = "prob")
+    pred$Predict <- predict(fit , newdata = data.test , type = "raw")
+    pred$Reference <- data.test$Class
+    
+    cat("Plot test results...")
+    cat("\n")
+    obj.roc <- roc(response=pred$Reference,predictor=pred[,1])
+    
+    p <- ggroc(obj.roc)+ggtitle(paste(model.label,"accuracy on test data")) +
+      theme_bw() + annotate("text",x = 0.1,y = 0.1,label=paste0("AUC=",round(obj.roc$auc,digits = 2)))
+    
+    tiff(filename = paste0(OutPrefix,".",model,".accuracy.on.test.data.tiff"),type="cairo", units = "in", height = 10,width = 10,res = 300)
+    print(p)
+    graphics.off()
+    
+    cat("Save results summary...")
+    cat("\n")
+    results.summ <- confusionMatrix(data =as.factor(pred$Predict) , reference = as.factor(pred$Reference))
+    sink(paste0(OutPrefix,".",model,".txt"))
+    cat(model,"(",model.label,")",sep = "",":\n")
+    print(results.summ)
+    sink()
+  },
+  error=function(e){
+    print(e)
+    })
+  cat("#########################################################################")
   cat("\n")
-  pred <- predict(fit , newdata = data.test , type = "prob")
-  pred$Predict <- predict(fit , newdata = data.test , type = "raw")
-  pred$Reference <- data.test$Class
-  
-  cat("Plot test results...")
-  cat("\n")
-  obj.roc <- roc(response=pred$Reference,predictor=pred[,1])
-  tiff(filename = paste0(OutPrefix,".",model,".accuracy.on.test.data.tiff"), units = "in", height = 6,width = 6,res = 300)
-  ggroc(obj.roc)+ggtitle(paste(model.label,"accuracy on test data")) +
-    theme_bw() + annotate("text",x = 0.1,y = 0.1,label=paste0("AUC=",round(obj.roc$auc,digits = 2)))
-  graphics.off()
-  cat("Save results summary...")
-  cat("\n")
-  sink(paste0(OutPrefix,".",model,".txt"))
-  cat(model,"(",model.label,")",sep = "",":\n")
-  results.summ <- confusionMatrix(data =as.factor(pred$Predict) , reference = as.factor(pred$Reference))
-  results.summ
-  sink()
 }
